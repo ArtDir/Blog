6c900b93aafb9ad4312ace8503718d52d262d211
ArtDir <effest@gmail.com>
2016-06-16 22:20:23 +0300

    Home page and delete posts
    Добавил кнопки на главой и возможность удалять посты

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
-  	@posts = Post.all.order("created_at DESC")
+	@posts = Post.all.order("created_at DESC")  	
   end
 
   def new
   	@post = Post.new
   end
+
   def create
   	@post = Post.new(post_params)
   	if @post.save
@@ -19,7 +22,28 @@ class PostsController < ApplicationController
   	@post = Post.find(params[:id])
   end
 
+  def edit
+  	@post = Post.find(params[:id])
+  end
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update(post_params)
+  		redirect_to @post
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	@post.destroy
+  	redirect_to root_path
+  end
+
   private
+
+  def find_post
+  	@post = Post.find(params[:id])
+  end
 
   def post_params
   	params.require(:post).permit(:title, :body)
